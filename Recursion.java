c0d618eb5201073bfcc7f157f52140ef1b453d53
Forest Hu <foresthu2006@gmail.com>
2015-04-16 17:40:00 -0700

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -5,39 +5,53 @@ package recurse;
  * @author Forest
  */
 public class Recursion {
-
+    
     public static void main(String[] args) {
         new Recursion().recurse(new Recurse("recurse"));
     }
     
-    Recurse.recurse recurse(Recurse recurse){
-        return recurse(recurse.recurse().recurse(recurse.recurse().recurse()+"recurse")); //what does it do?!?!?1
+    Recurse.recurse recurse(Recurse recurse) {
+        return recurse(recurse.recurse(recurse).recurse(recurse.recurse(recurse).recurse(recurse) + recurse.recurse(recurse).recurse(recurse.recurse(recurse).Recurse(recurse), recurse.recurse(recurse).Recurse(recurse.recurse(recurse))))); //what does it do?!?!?1
     }
     
 }
+
 class Recurse {
+
     String recurse;
     
-    public Recurse(String recurse){
+    public Recurse(String recurse) {
         this.recurse = recurse;
     }
     
-    public String Recurse(){
+    public String Recurse() {
         return this.recurse;
     }
     
-    public recurse recurse(){
-        return new recurse();
+    public recurse recurse(Recurse recurse) {
+        return recurse.new recurse();
     }
     
-    public class recurse{
-    
-        public Recurse recurse(String recurse){
-            return new Recurse(recurse()+recurse);
+    public class recurse {
+        
+        public Recurse recurse(String recurse) {
+            return new Recurse(Recurse.this.recurse + recurse);
         }
         
-        public String recurse(){
-            return Recurse.this.Recurse();
+        public String recurse(Recurse recurse) {
+            return recurse.Recurse();
+        }
+        
+        public String recurse(String recurse, String Recurse) {
+            return recurse + Recurse;
+        }
+        
+        public String Recurse(Recurse recurse) {
+            return "Hello ";
+        }
+        
+        public String Recurse(Recurse.recurse recurse) {
+            return "World! ";
         }
     }
 }
