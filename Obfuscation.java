599254ef688338e55c4b53b4f89ef24263d6fa4c
teamcsquared <noreply@teamcsquared.us>
2015-05-11 23:19:07 -0700

    Update Obfuscation.java

--- Obfuscation.java
+++ Obfuscation.java
@@ -1,14 +1,13 @@
-
 public class Obfuscation {
     
     public static void main(String[] args) {
-        new Obfuscation().obfuscate(new Obfuscate(""));
+        new Obfuscation().obfuscate(new Obfuscate("bit.ly/"));
     }
     
-    //what does the longest version of the string in line 18 contain after a call of the following method, provided that it does not recurse?
-    //the code has been edited so that it will not compile properly to prevent people from editing the code and running it.
+    //what does the longest version of the string in line 17 contain after a call of the following method, provided that it does not recurse?
+    //note that some code has been redacted so that it will not compile properly to prevent people from simply running the code.
     Obfuscate.obfuscate obfuscate(Obfuscate obfuscate) {
-        return obfuscate(obfuscate.obfuscate(obfuscate).obfuscate(obfuscate.obfuscate(obfuscate).obfuscate(obfuscate) + obfuscate.obfuscate(obfuscate).obfuscate(obfuscate.obfuscate(obfuscate).Obfuscate(obfuscate), obfuscate.obfuscate(obfuscate).Obfuscate("obfuscat3 "), obfuscate.obfuscate(obfuscate).Obfuscate(obfuscate.obfuscate(obfuscate))))); //what does it do?!?!?1
+        return obfuscate(obfuscate.obfuscate(obfuscate).obfuscate(obfuscate.obfuscate(obfuscate).obfuscate(obfuscate) + obfuscate.obfuscate(obfuscate).obfuscate() + obfuscate.obfuscate(obfuscate).obfuscate(obfuscate.obfuscate(obfuscate).Obfuscate(obfuscate), obfuscate.obfuscate(obfuscate).Obfuscate(), obfuscate.obfuscate(obfuscate).Obfuscate(obfuscate.obfuscate(obfuscate))))); 
     }
     
 }
@@ -32,7 +31,7 @@ class Obfuscate {
     public class obfuscate {
         
         public Obfuscate obfuscate(String obfuscate) {
-            return new Obfuscate(Obfuscate.this.obfuscate + obfuscate);
+            return new Obfuscate(obfuscate);
         }
         
         public String obfuscate(Obfuscate obfuscate) {
@@ -40,19 +39,40 @@ class Obfuscate {
         }
         
         public String obfuscate(String obfuscate, String Obfuscate, String Obfuscat3) {
-            return [redacted]; //might want to read up on some activities
+            return Obfuscat3 + obfuscate + Obfuscate; 
         }
         
-        public String Obfuscate(String obfuscate){
-        	return obfuscate;
+        public String obfuscate(){
+            return joaat("73302823");
+        }
+        
+        public String Obfuscate(){
+            return md5("6bd48b1e57856137037bfee4dec8d57f");
         }
         
         public String Obfuscate(Obfuscate obfuscate) {
-            return "Obfuscate ";
+            return sha1("81b9c8d478dc69e16835b1b3b8d29cb69ffb54bc");
         }
         
         public String Obfuscate(Obfuscate.obfuscate obfuscate) {
-            return "obfuscate ";
+            return haval128_3("ff8b5e29dff96ac8afdf12e4d8c9bc08");
         }
+        
+        public String haval128_3(String haval128_3){
+            return [redacted];
+        }
+        
+        public String joaat(String joaat){
+            return [redacted];
+        }
+        
+        public String md5(String md5){
+            return [redacted];
+        }
+        
+        public String sha1(String sha1){
+            return [redacted];
+        }
+        
     }
 }
